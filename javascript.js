3f35ba09e698b60864b8a1e4d887fbfade0dd633
Juan <juandavc12@hotmail.com>
2020-08-17 19:09:38 -0500

    First jQuery

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
-const $input = document.querySelector("#newM")
-const $send = document.querySelector("#sendButton")
-const $chat = document.querySelector(".msg")
+$(document).ready(function () {
 
-$send.addEventListener('click', () => {
+const $input = $("#newM");
+const $send = $("#sendButton");
+const $chat = $('.msg');
+
+$($send).on('click', () => {
     event.preventDefault();
-    
-    const value = $input.value;
 
+    let value = $input.val();
+    
     if (value !== '') {
         const now = new Date(); //Horario actual
 
@@ -22,56 +24,142 @@ $send.addEventListener('click', () => {
         printTemplate(message)
         messages.push(message)
 
-        $input.value="";
+        $input.val("");
 
     }
 
 });
 
-
 const messages = [
-    {
-        time: "5:21",
-        content: "Hola, ¿Como estan?",
-        author: {
-            name: "Pedro Pepino Papas",
-            image: "images.jpg",
+        {
+            time: "5:21",
+            content: "Hola, ¿Como estan?",
+            author: {
+                name: "Pedro Pepino Papas",
+                image: "images.jpg",
+            },
         },
-    },
-
-    {
-        time: "5:28",
-        content: "Bien",
-        author: {
-            name: "Elsa Capuntas",
-            image: "images.jpg",
+    
+        {
+            time: "5:28",
+            content: "Bien",
+            author: {
+                name: "Elsa Capuntas",
+                image: "images.jpg",
+            },
         },
-    },
-    {
-        time: "5:40",
-        content: "Todo excelente",
-        author: {
-            name: "Sumer Ceso",
-            image: "images.jpg",
+        {
+            time: "5:40",
+            content: "Todo excelente",
+            author: {
+                name: "Sumer Ceso",
+                image: "images.jpg",
+            },
         },
-    },
-
-];
-
+    
+    ];
 
 function printTemplate(message){
-const { author, time, content} = message //destructing
+    const { author, time, content} = message //destructing
 
     const template = `<li class="chatBox">
-        <img class="imgContact" src="${author.image}">
-        <div class="nameContact"><strong>${author.name}<span class="sendHour">${time}</span></strong></div>        
-        <p class="content">${content}</p>
-        </li>`; 
- 
+         <img class="imgContact" src="${author.image}">
+         <div class="nameContact"><strong>${author.name}<span class="sendHour">${time}</span></strong></div>        
+         <p class="content">${content}</p>
+         </li>`;
+         
 
-$chat.innerHTML += template
+         $($chat).append(template)
 }
 
-    messages.forEach((message) => {
-      printTemplate(message);  
-    });
+messages.forEach((message) => {
+          printTemplate(message);  
+        });
+
+});
+
+
+
+
+
+//_________________________________________________________
+
+
+
+//     const $input = document.querySelector("#newM")
+// const $send = document.querySelector("#sendButton")
+// const $chat = document.querySelector(".msg")
+
+// $send.addEventListener('click', () => {
+//     event.preventDefault();
+    
+//     const value = $input.value;
+
+//     if (value !== '') {
+//         const now = new Date(); //Horario actual
+
+//         const message = {
+//             time: `${now.getHours()}:${now.getMinutes()}`,
+//         content: value,
+//         author: {
+//             name: "Pedro Pepino Papas",
+//             image: "images.jpg",
+//         },
+//     };
+
+//         printTemplate(message)
+//         messages.push(message)
+
+//         $input.value="";
+
+//     }
+
+// });
+
+
+// const messages = [
+//     {
+//         time: "5:21",
+//         content: "Hola, ¿Como estan?",
+//         author: {
+//             name: "Pedro Pepino Papas",
+//             image: "images.jpg",
+//         },
+//     },
+
+//     {
+//         time: "5:28",
+//         content: "Bien",
+//         author: {
+//             name: "Elsa Capuntas",
+//             image: "images.jpg",
+//         },
+//     },
+//     {
+//         time: "5:40",
+//         content: "Todo excelente",
+//         author: {
+//             name: "Sumer Ceso",
+//             image: "images.jpg",
+//         },
+//     },
+
+// ];
+
+
+// function printTemplate(message){
+// const { author, time, content} = message //destructing
+
+//     const template = `<li class="chatBox">
+//         <img class="imgContact" src="${author.image}">
+//         <div class="nameContact"><strong>${author.name}<span class="sendHour">${time}</span></strong></div>        
+//         <p class="content">${content}</p>
+//         </li>`; 
+ 
+
+// $chat.innerHTML += template
+// }
+
+//     messages.forEach((message) => {
+//       printTemplate(message);  
+//     });
