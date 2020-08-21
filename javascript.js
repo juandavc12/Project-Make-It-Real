2c8c75d4f5a1d6919fa8c455695e65077de17b6b
Juan <juandavc12@hotmail.com>
2020-08-21 18:21:08 -0500

    Conect to server

--- javascript.js
+++ javascript.js
@@ -1,32 +1,39 @@
 $(document).ready(function () {
 
 $('#listchan li').mouseover(function () {
-    $(this).css('color', 'white')
+    $(this).css({'color': 'white', 'background-color': 'black', 'margin-color': 'black'})
+
 });
 
 $('#listchan li').mouseout(function () {
-    $(this).css('color', '#757477')
+    $(this).css({'color': '#757477', 'background-color': 'transparent'})
 });
 
+
+function scrollDown (timeAnimation) {
+    $(".msg").animate({ scrollTop: $('.msg')[0].scrollHeight}, timeAnimation);
+}
 
 const $input = $("#newM");
 const $send = $("#sendButton");
 const $chat = $('.msg');
 
-$($send).on('click', () => {
+$send.on('click', () => {
     event.preventDefault();
-
+    scrollDown(1000);
+   
     let value = $input.val();
     
     if (value !== '') {
         const now = new Date(); //Horario actual
-
+        const minutes = `${now.getMinutes()}`
+        
         const message = {
-            time: `${now.getHours()}:${now.getMinutes()}`,
-        content: value,
-        author: {
-            name: "Pedro Pepino Papas",
-            image: "images.jpg",
+            time: `${now.getHours()}:${minutes}`,
+            content: value,
+            author: {
+                name: "Pedro Pepino Papas",
+                image: "images.jpg",
         },
     };
 
@@ -34,9 +41,10 @@ $($send).on('click', () => {
         messages.push(message)
 
         $input.val("");
+        console.log(messages)
 
     }
-
+ 
 });
 
 const messages = [
@@ -81,11 +89,33 @@ function printTemplate(message){
          
 
          $($chat).append(template)
+         scrollDown(10);
 }
 
-messages.forEach((message) => {
+        messages.forEach((message) => {
           printTemplate(message);  
         });
+
+
+
+$.get("https://jsonplaceholder.typicode.com/comments").then((response) => {
+        response.forEach((result) => {
+            const now = new Date();
+            const message = {
+                time: `${now.getHours()}:${now.getMinutes()}`,
+                content: result.body,
+                author: {
+                    name: result.name,
+                    image: "images.jpg",
+            },
+        };
+        
+        printTemplate(message)
+        messages.push(message);
+        });
+
+       
+});
 
 });
 
